84f571c382d42877a866d231b83b3d72f61e2983
Mike Graves <mgraves@mit.edu>
2019-09-20 11:09:48 -0400

    Pin modules to 0.11

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Define composite variables for resources
 module "label" {
-  source = "git::https://github.com/mitlibraries/tf-mod-name?ref=master"
+  source = "github.com/mitlibraries/tf-mod-name?ref=0.11"
   name   = "${var.name}"
   tags   = "${var.tags}"
 }
